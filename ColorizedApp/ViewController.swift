9f627a3a55e8900c4a95da44c0c5a0e4c46c9a5e
Rustam S <blakfoh@gmail.com>
2023-05-23 15:19:29 +0600

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Rustam Samiev on 22.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
