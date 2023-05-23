a8d57a2b5adc9617b580c8a2547d590f783a06ad
Rustam S <blakfoh@gmail.com>
2023-05-23 22:55:52 +0600

    показываю данные слайдера в лейблах, задаю бэкграунд прямоугольника по данным слайдера

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,11 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var rectangleBlock: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSliderProperty: UISlider!
+    @IBOutlet var greenSliderProperty: UISlider!
+    @IBOutlet var blueSliderProperty: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rectangleBlock.layer.cornerRadius = 16
+        redLabel.text = String(Int(redSliderProperty.value))
+        greenLabel.text = String(Int(greenSliderProperty.value))
+        blueLabel.text = String(Int(blueSliderProperty.value))
+        
+        rectangleBlock.backgroundColor = UIColor(
+            red: CGFloat(Int(redSliderProperty.value)) / 255.0,
+            green: CGFloat(Int(greenSliderProperty.value)) / 255.0,
+            blue: CGFloat(Int(blueSliderProperty.value)) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 
+    // MARK: - IBActions
+    @IBAction func redSlider() {
+        redLabel.text = String(Int(redSliderProperty.value))
+    }
+    
+    @IBAction func greenSlider() {
+        greenLabel.text = String(Int(greenSliderProperty.value))
+    }
+    
+    @IBAction func blueSlider() {
+        blueLabel.text = String(Int(blueSliderProperty.value))
+    }
 }
 
