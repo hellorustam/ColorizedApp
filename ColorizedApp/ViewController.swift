152f53d691bbedf3d95561dd0d1befd8217c4303
Rustam S <blakfoh@gmail.com>
2023-05-23 23:45:02 +0600

    добавил функцию копирования HEX цвета

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -18,13 +18,9 @@ class ViewController: UIViewController {
     @IBOutlet var greenSliderProperty: UISlider!
     @IBOutlet var blueSliderProperty: UISlider!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        rectangleBlock.layer.cornerRadius = 16
-        redLabel.text = String(Int(redSliderProperty.value))
-        greenLabel.text = String(Int(greenSliderProperty.value))
-        blueLabel.text = String(Int(blueSliderProperty.value))
-        
+    @IBOutlet var copyButton: UIButton!
+    
+    private func setBackgroundColor() {
         rectangleBlock.backgroundColor = UIColor(
             red: CGFloat(Int(redSliderProperty.value)) / 255.0,
             green: CGFloat(Int(greenSliderProperty.value)) / 255.0,
@@ -32,18 +28,44 @@ class ViewController: UIViewController {
             alpha: CGFloat(1.0)
         )
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rectangleBlock.layer.cornerRadius = 16
+        redLabel.text = String(Int(redSliderProperty.value))
+        greenLabel.text = String(Int(greenSliderProperty.value))
+        blueLabel.text = String(Int(blueSliderProperty.value))
+        
+        copyButton.titleLabel?.text = ""
+        
+        setBackgroundColor()
+    }
 
     // MARK: - IBActions
     @IBAction func redSlider() {
         redLabel.text = String(Int(redSliderProperty.value))
+        setBackgroundColor()
+        copyButton.titleLabel?.text = ""
     }
     
     @IBAction func greenSlider() {
         greenLabel.text = String(Int(greenSliderProperty.value))
+        setBackgroundColor()
+        copyButton.titleLabel?.text = ""
     }
     
     @IBAction func blueSlider() {
         blueLabel.text = String(Int(blueSliderProperty.value))
+        setBackgroundColor()
+        copyButton.titleLabel?.text = ""
+    }
+    
+    @IBAction func copyColorRectangle() {
+        copyButton.titleLabel?.text = "Copied HEX color"
+        
+        UIPasteboard.general.string = "#" + String(format:"%02X", Int(redSliderProperty.value)) +
+        String(format:"%02X", Int(greenSliderProperty.value)) +
+        String(format:"%02X", Int(blueSliderProperty.value))
     }
 }
 
